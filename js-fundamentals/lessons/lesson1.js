3f0d831f07a29fe0465697be444e5019a3e2878f
AghaAmil <moravveji.amir.92@gmail.com>
2024-04-05 14:59:39 +0330

    javascript hello world

---
+++ js-fundamentals/lessons/lesson1.js
@@ -0,0 +1,35 @@
+/* 
+
+********************************************
+********* First Lesson Hello World *********
+********************************************
+
+*/
+
+// double slash - single line comment
+
+/*
+
+this is a
+multi-line 
+comment
+
+*/
+
+// single quotation
+console.log("Hello World!");
+
+// double quotation
+console.log("This is a text inside the double quote");
+
+// using special quotation inside our text
+console.log("I'm going to work everyday.");
+console.log('I live in "Tehran" city');
+
+// using backslash for quotations
+console.log("I live in \"Pasdaran\" neighborhood.")
+
+
+// logging a numbers
+console.log(34);
+console.log(83.3);
