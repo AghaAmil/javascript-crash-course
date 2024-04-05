e5482b1146e3f0613cee0c5a59b2c86c2886abfc
AghaAmil <moravveji.amir.92@gmail.com>
2024-04-05 15:05:42 +0330

    running js using browser

---
+++ js-fundamentals/lessons/running-js-differently/hello-world.js
@@ -0,0 +1,20 @@
+console.log("Hello World!");
+
+// adding the items from the previous exercise
+
+// single quotation
+console.log("Hello World!");
+
+// double quotation
+console.log("This is a text inside the double quote");
+
+// using special quotation inside our text
+console.log("I'm going to work everyday.");
+console.log('I live in "Tehran" city');
+
+// using backslash for quotations
+console.log('I live in "Pasdaran" neighborhood.');
+
+// logging a numbers
+console.log(34);
+console.log(83.3);
