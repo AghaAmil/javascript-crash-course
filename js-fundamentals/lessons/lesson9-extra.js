413dc9f6bb099c30d626611100544dcd55e5478e
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2024-06-10 18:08:57 +0330

    lesson 9 extra materials

---
+++ js-fundamentals/lessons/lesson9-extra.js
@@ -0,0 +1,66 @@
+/*
+
+********************************************
+******* Lesson 8 - Arrow Functions *********
+********************************************
+
+*/
+
+// simple form of functions
+function greeting() {
+  console.log("Hello World!");
+}
+
+let displayPi = function () {
+  const PI = 3.14;
+  console.log(`PI Number = ${PI}`);
+};
+
+greeting();
+displayPi();
+
+// blank line
+console.log();
+
+// arrow function format of above functions
+const sayHello = () => {
+  console.log("Hello World!");
+};
+
+const sayHello2 = () => "Hello World!";
+
+const arrowDisplayPi = () => {
+  const PI = 3.14;
+  console.log(`PI Number = ${PI}`);
+};
+
+console.log(sayHello2());
+arrowDisplayPi();
+
+// blank line
+console.log();
+
+// an arrow function to add two numbers
+const addNumbers = (a, b) => a + b;
+
+// call the function with two numbers
+const result = addNumbers(5, 3);
+console.log(result);
+
+// constructor function
+function Person() {
+  (this.name = "Jack"),
+    (this.age = 25),
+    (this.sayName = function () {
+      console.log(this.age);
+
+      let innerFunc = () => {
+        console.log(this.age);
+      };
+
+      innerFunc();
+    });
+}
+
+const x = new Person();
+x.sayName();
