db876917ca31a82d30f6aa55d7e8bee2724ff2af
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2024-06-10 17:16:18 +0330

    object extra materials

--- js-fundamentals/lessons/lesson5-extra.js
+++ js-fundamentals/lessons/lesson5-extra.js
@@ -8,7 +8,8 @@
 
 /*
 
-Basic Object
+Basic Object with Method
+A JavaScript method is a function defined within an object.
 
 */
 
@@ -21,13 +22,70 @@ const student = {
   },
 };
 
-console.log(student.fname);
-console.log(student["uid"]);
+console.log(`Object first name is : ${student.fname}`);
+console.log(`Object User ID is : ${student["uid"]}`);
 student.intro();
+
+// blank line
+console.log();
 
 /*
 
-JavaScript Method
-A JavaScript method is a function defined within an object.
+JavaScript this Keyword
+We use this keyword in an object method to access a property of the same object.
 
 */
+
+// modify the object
+student.fname = "Amir";
+student.lname = "Fallah Sohy";
+console.log(student);
+student.intro();
+
+// blank line
+console.log();
+
+//adding a method to an object
+student.adaptiveIntro = function () {
+  console.log(`Our new student is ${this.fname} ${this.lname}`);
+};
+
+console.log(student);
+student.adaptiveIntro();
+
+// blank line
+console.log();
+
+/*
+
+JavaScript Constructor Function
+
+*/
+
+// constructor function with parameters
+function person(fullname, yearBorn, gender) {
+  (this.fname = fullname),
+    (this.yearBorn = yearBorn),
+    (this.gender = gender),
+    (this.greet = function () {
+      return `Hello ${this.fname}`;
+    });
+
+  this.age = function () {
+    return 2024 - this.yearBorn;
+  };
+}
+
+const person1 = new person("Amirhossein Moravveji", 1993, "Male");
+const person2 = new person("Sara Rahmani", 1995, "Female");
+
+console.log(person1);
+console.log(person1.greet());
+console.log(`The person1 age is ${person1.age()}`);
+
+// blank line
+console.log();
+
+console.log(person2);
+console.log(person2.greet());
+console.log(`The person1 age is ${person2.age()}`);
