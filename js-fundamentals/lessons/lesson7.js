7d3ddd7011649f104e070d1ed44e85368cd63221
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2024-06-20 20:48:15 +0330

    extra material for lesson 7

--- js-fundamentals/lessons/lesson7.js
+++ js-fundamentals/lessons/lesson7.js
@@ -28,6 +28,31 @@ if (yourMark >= 90) {
 // blank line
 console.log();
 
+// Nested if...else Statement
+let marks = 60;
+
+// outer if...else statement
+// student passed if marks 40 or above
+// otherwise, student failed
+
+if (marks >= 40) {
+  // inner if...else statement
+  // Distinction if marks is 80 or above
+
+  if (marks >= 80) {
+    console.log("Distinction");
+  } else {
+    console.log("Passed");
+  }
+} else {
+  console.log("Failed");
+}
+
+// Output: Passed
+
+// blank line
+console.log();
+
 // sample 2
 let timeOfTheDay = 19;
 
@@ -39,6 +64,52 @@ if (timeOfTheDay >= 6 && timeOfTheDay <= 12) {
   console.log("Good Night To You");
 } else {
   console.log("Enter a Valid Time");
+}
+
+// blank line
+console.log();
+
+/*
+
+switch case statement
+
+*/
+
+// Suppose we want to display a message based on the current day of the week
+let day = 3;
+let activity;
+
+switch (day) {
+  case 1:
+    console.log("Sunday");
+    break;
+
+  case 2:
+    console.log("Monday");
+    break;
+
+  case 3:
+    console.log("Tuesday");
+    break;
+
+  case 4:
+    console.log("Wednesday");
+    break;
+
+  case 5:
+    console.log("Thursday");
+    break;
+
+  case 6:
+    console.log("Friday");
+    break;
+
+  case 7:
+    console.log("Saturday");
+    break;
+
+  default:
+    console.log("Invalid Day");
 }
 
 // switch statement
@@ -60,5 +131,3 @@ switch (trafficLight) {
 }
 
 console.log(message);
-
-// Output: Proceed or continue driving.
