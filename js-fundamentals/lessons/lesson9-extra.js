7165349a90248766b1d4ee7b2c67fbcdfd5983a3
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2024-06-27 20:30:27 +0330

    correction on constructor func

--- js-fundamentals/lessons/lesson9-extra.js
+++ js-fundamentals/lessons/lesson9-extra.js
@@ -49,17 +49,17 @@ console.log(result);
 
 // constructor function
 function Person() {
-  (this.name = "Jack"),
-    (this.age = 25),
-    (this.sayName = function () {
+  this.name = "Jack";
+  this.age = 25;
+  this.sayName = function () {
+    console.log(this.age);
+
+    let innerFunc = () => {
       console.log(this.age);
+    };
 
-      let innerFunc = () => {
-        console.log(this.age);
-      };
-
-      innerFunc();
-    });
+    innerFunc();
+  };
 }
 
 const x = new Person();
